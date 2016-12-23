7a246e02bc9746b6190e2459d7718760fce4ce17
nick1025 <yhl1025@163.com>
2016-12-23 17:05:05 +0800

    doc
    doc

---
+++ doc/api/web-api/api.js
@@ -0,0 +1,24 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "App",
+        "AsideComp",
+        "ChartComp",
+        "FooterComp",
+        "HeaderComp",
+        "ListComp",
+        "MainComp",
+        "TestComponent",
+        "common.Ajax",
+        "common.CommonUtil",
+        "common.Component",
+        "common.Core",
+        "common.EventBus",
+        "common.History",
+        "common.UIHelper"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
